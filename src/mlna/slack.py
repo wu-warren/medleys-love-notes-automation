a7dc8d3744988963d5785d90cf79ddc86b6affff
Eric Wang <89937277+nouturnsign@users.noreply.github.com>
2025-10-01 17:53:23 -0700

    Switch to App

--- src/mlna/slack.py
+++ src/mlna/slack.py
@@ -5,18 +5,18 @@ import os
 from dotenv import load_dotenv
 
 from slack_bolt.adapter.fastapi import SlackRequestHandler
-from slack_bolt.async_app import AsyncApp
+from slack_bolt.app import App
 
 __all__ = ("app", "handler")
 
 load_dotenv()
 
-app = AsyncApp(token=os.environ["SLACK_BOT_TOKEN"], signing_secret=os.environ["SLACK_SIGNING_SECRET"])
+app = App(token=os.environ["SLACK_BOT_TOKEN"], signing_secret=os.environ["SLACK_SIGNING_SECRET"])
 handler = SlackRequestHandler(app)
 
 
 @app.command("/hello")
-async def hello(ack, say, command):
-    await ack()
+def hello(ack, say, command):
+    ack()
     user_id = command["user_id"]
-    await say(f"Hello, <@{user_id}>! You sent the command: `{command['command']}` with text: `{command['text']}`")
+    say(f"Hello, <@{user_id}>! You sent the command: `{command['command']}` with text: `{command['text']}`")
